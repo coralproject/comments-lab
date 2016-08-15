1aa3a256b0444a2c9d36afe79f1bed0cc40b4c04
David Jay <davidgljay@gmail.com>
2016-08-15 15:19:51 -0400

    Adding styles and date to defaultComment

--- src/components/comments/DefaultComment.jsx
+++ src/components/comments/DefaultComment.jsx
@@ -1,11 +1,14 @@
-import React, {Component, PropTypes} from 'react'; 
+import React, {Component, PropTypes} from 'react';
+import moment from 'moment';
 
 class DefaultComment extends Component {
   render() {
+    const styles = this.props.styles || defaultStyles;
 
     return (
       <div className="comment">
-        <div className="content">
+        <div style={ styles.commentDate } className="commentDate">{ moment().fromNow() }</div>
+        <div className="commentContent" style={ styles.commentContent} >
           {this.props.content}
         </div>
       </div>
@@ -18,3 +21,15 @@ DefaultComment.propTypes = {
 };
 
 export default DefaultComment;
+
+let defaultStyles =   {
+  commentDate: {
+    fontSize: '10pt',
+    color: '#999',
+    marginBottom: '5px'
+  },
+  commentContent: {
+    fontSize: '12pt',
+    minHeight: '60px'
+  }
+};
