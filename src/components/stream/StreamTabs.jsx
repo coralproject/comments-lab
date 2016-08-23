d365722001591060a4a31cd91685704d2e20930e
David Jay <davidgljay@gmail.com>
2016-08-23 17:08:38 -0400

    Left justifying tabs.

--- src/components/stream/StreamTabs.jsx
+++ src/components/stream/StreamTabs.jsx
@@ -21,15 +21,15 @@ class StreamTabs extends Component {
 
   render() {
     let styles = this.props.styles || defaultStyles;
-    return <div className="mdl-tabs mdl-js-tabs is-upgraded">
-      <div className="mdl-tabs__tab-bar">
+    return <div className="mdl-tabs mdl-js-tabs is-upgraded" >
+      <div className="mdl-tabs__tab-bar" style={styles.tabBar}>
         <a className={'mdl-tabs__tab ' + (this.props.activeTab == 'all' ? 'is-active' : null)}
-          style={styles.streamTabs}
+          style={styles.streamTab}
           onClick={ this.onAllClick.bind(this) }>
           All
         </a>
         <a className={'mdl-tabs__tab ' + (this.props.activeTab == 'staff' ? 'is-active' : null)}
-          style={styles.streamTabs}
+          style={styles.streamTab}
           onClick={ this.onStaffClick.bind(this) }>
           Staff Picks
         </a>
@@ -39,28 +39,11 @@ class StreamTabs extends Component {
 }
 
 const defaultStyles = {
-  streamTabs: {
-    cursor:'pointer'
+  tabBar: {
+    justifyContent:'flex-start'
   },
   streamTab: {
-    borderTop: '3px solid #ccc',
-    borderRight: '1px solid #ddd',
-    borderLeft: '1px solid #ddd',
-    borderBottom: '1px solid #ddd',
-    padding: '15px',
-    fontSize: '14pt',
-    fontWeight: '600',
-    background: '#f0f0f0',
-    marginTop: '10px',
-    marginRight: '-1px',
-    marginBottom: '-1px',
-    cursor: 'pointer',
-    outline: 'none'
-  },
-  activeTab: {
-    borderTop: '3px solid red',
-    borderBottom: '1px solid white',
-    background: 'white'
+    cursor:'pointer'
   }
 };
 
