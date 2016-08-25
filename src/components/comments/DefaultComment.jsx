03d4eb146ef7084863a49b7fef2dc012fc26a3ed
David Jay <davidgljay@gmail.com>
2016-08-25 12:34:25 -0400

    Making room for menu in comment

--- src/components/comments/DefaultComment.jsx
+++ src/components/comments/DefaultComment.jsx
@@ -4,52 +4,6 @@ import ReactEmoji from 'react-emoji';
 
 class DefaultComment extends Component {
 
-
-    // filterContent(content) {
-    //   /*
-    //     This pattern searches for tags within comment content,
-    //     those tags relate to the Playground's settings as in:
-
-    //     {moderation.muting}...something...{/moderation.muting}
-
-    //     So we can show some mutations inside the content.
-    //   */
-    //   let tagSearchPattern = /{(.+)}(.*){\/(\1)}/;
-    //   let matches = content.match(tagSearchPattern);
-    //   let filteredContent = content;
-    //   if (matches) {
-    //     let togglerGroupAndKey = matches[1].split('.');
-    //     if (this.props.togglerGroups[togglerGroupAndKey[0]].togglers[togglerGroupAndKey[1]].status) {
-    //       filteredContent = content.replace('{' + matches[1] + '}' + matches[2] + '{/' + matches[1] + '}', matches[2]);
-    //     } else {
-    //       filteredContent = content.replace('{' + matches[1] + '}' + matches[2] + '{/' + matches[1] + '}', '');
-    //     }
-    //     return this.filterContent(filteredContent);
-    //   } else {
-    //     return filteredContent;
-    //   }
-    // }
-
-  // This function should prepare the content to be rendered as HTML
-  // within a 'dangerouslySetInnerHTML' call.
-  // formatContent(content) {   
-  //   if (this.props.showEmoji) {
-  //     content = ReactEmoji.emojify(content);
-  //     // After running emojify, we get an array of strings (which may contain HTML)
-  //     // and objects holding the Emojis
-  //     content = content.map((obj) => {
-  //       if (typeof obj == 'object') {
-  //         // We render the Emojis as plain HTML, 
-  //         // or they would render as "[Object object]"
-  //         return React.renderToString(<span>{ obj }</span>);
-  //       } else {
-  //         return obj;
-  //       }
-  //     }).join('');
-  //   }
-  //   return { __html: content };
-  // }
-
   render() {
     const styles = this.props.styles || defaultStyles;
 
@@ -76,6 +30,7 @@ let defaultStyles =   {
   },
   commentContent: {
     fontSize: '12pt',
-    marginBottom: '20px'
+    marginBottom: '20px',
+    width:'95%'
   }
 };
