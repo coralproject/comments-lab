670cd4cf1df80b914539a2dd141e96610e211285
David Jay <davidgljay@gmail.com>
2016-09-08 00:03:50 -0400

    Adding Reaction selection

--- src/components/interactions/Reactions.jsx
+++ src/components/interactions/Reactions.jsx
@@ -1,17 +1,40 @@
 import React, {Component, PropTypes} from 'react';
 import ReactEmoji from 'react-emoji';
+import { Icon } from 'react-mdl';
 import {updateItem} from 'playground/PlaygroundActions';
+import EmojiPicker from 'react-emoji-picker';
+import emojiMap from 'react-emoji-picker/lib/emojiMap';
+
 
 class Reactions extends Component {
 
   constructor(props) {
     super(props);
     this.reactionClick = this.reactionClick.bind(this);
+    this.toggleEmojiPicker = this.toggleEmojiPicker.bind(this);
+    this.onSelectEmoji = this.onSelectEmoji.bind(this);
+    this.state = {
+      showEmojiPicker:false
+    };
   }
 
   static propTypes = {
     reactions: PropTypes.array.isRequired,
     id: PropTypes.string.isRequired
+  }
+
+  toggleEmojiPicker() {
+    this.setState({ showEmojiPicker: !this.state.showEmojiPicker });
+  }
+
+  onSelectEmoji(emoji) {
+    let reactions = this.props.reactions.slice();
+    reactions.push({
+      name: emoji,
+      count:1
+    });
+    this.props.dispatch(updateItem(this.props.id,'comments','reactions',reactions));
+    this.setState({showEmojiPicker:false});
   }
 
   reactionClick(name) {
@@ -37,11 +60,24 @@ class Reactions extends Component {
       {
         this.props.reactions.map((reaction) => {
           return <div className='reactionEmoji' style={styles.reactionEmoji} key={reaction.name} onClick = {this.reactionClick(reaction.name)}> 
-              {ReactEmoji.emojify(':' + reaction.name + ':')}
+              {ReactEmoji.emojify(reaction.name)}
               <span style={styles.interactionCount} className='interactionCount'>{reaction.count}</span>
             </div>;
 
         })
+      }
+        <div      
+          style={styles.toggleEmojiPicker}
+          className="toggleEmojiPicker">
+          <Icon
+            onClick={ this.toggleEmojiPicker }
+            name="insert_emoticon"/>
+        </div>
+      {
+        this.state.showEmojiPicker &&
+          <EmojiPicker
+          style={styles.emojiPickerStyles} onSelect={this.onSelectEmoji}
+          />
       }
     </div>;
   }
@@ -60,5 +96,17 @@ const defaultStyles = {
   },
   interactionCount:{
     fontSize:10
+  },
+  emojiPickerStyles: {
+    position: 'relative',
+    backgroundColor: 'white',
+    width: '100%',
+    borderTop: 'none',
+    zIndex: '9999'
+  },
+  toggleEmojiPicker:{
+    padding:1,
+    cursor:'pointer',
+    display:'inline-block'
   }
 };
