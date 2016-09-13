191dce0bd202e28d1a974912443689d44dfd2fe8
David Jay <davidgljay@gmail.com>
2016-09-12 23:54:05 -0400

    Adding new comment copy

--- src/playground/PlayGroundReducer.js
+++ src/playground/PlayGroundReducer.js
@@ -11,7 +11,7 @@ const initialState = {
     users:users
   },
   config:config,
-  stream:['a','b','c','d','e','f','g','h'],
+  stream:['a','b','c','d','f','g','h','i'],
   showWelcome:true,
   numChars:100,
   snackbar:{}
