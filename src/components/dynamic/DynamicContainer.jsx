2c26e66db8a73639bd554947df038d6e6c48875e
David Jay <davidgljay@gmail.com>
2016-09-20 11:58:33 -0400

    Handling one to many relationships in graphql

--- src/components/dynamic/DynamicContainer.jsx
+++ src/components/dynamic/DynamicContainer.jsx
@@ -14,35 +14,74 @@ class DynamicContainer extends Component {
     id: PropTypes.string.isRequired
   }
 
-  getPropsFromItems(query) {
-    let idStack = [this.props.id];
+  traverseEdges(edges, index, query) {
+    let bracketCount = 0;
+    let subqueryLength = 0;
+    let subquery = query.slice(index).reduce((subquery, char, i) => {
+      if (bracketCount === 0 && i !== 0) {
+        return subquery;
+      }
+      subqueryLength ++;
+      switch (char) {
+      case '{':
+        bracketCount ++;
+        break;
+      case '}':
+        bracketCount --;
+        break;
+      }
+      return subquery += char;
+    },'');
+    query.splice(index, subqueryLength-1);
+    return edges.reduce((array, edge) => {
+      array.push(this.getPropsFromItems(subquery,edge));
+      return array;
+    },[]);
+  }
+
+  getPropsFromItems(query, id) {
+    let idStack = [id];
     let relationshipStack = [];
     let result = {};
 
-
-
-    query.split('').reduce((string, char) => {
+    query.split('').reduce((string, char, i, q) => {
       const id = idStack[idStack.length - 1];
       let object = relationshipStack.reduce((object, relationship) => {
         return object[relationship];
       }, result);
-      if (/[a-z0-9]/i.exec(char)) {
+      if (/[a-z0-9]/i.test(char)) {
         return string + char;
       }
-      if (char === '{') {
-        if (!string) return string;
+      switch (char) {
+      case '{':
+        if (!string) {
+          return string;
+        }
         idStack.push(this.props.items[id][string]);
         relationshipStack.push(string);
-        object[string] = {};
-      }
-      if (char === '}') {
-        if (!string) return string; 
-        object[string] = this.props.items[id][string];
+        let val = this.props.items[id][string]; 
+        if (val.constructor === Array) {
+          object[string] = this.traverseEdges(val, i, q);
+          idStack.pop();
+          relationshipStack.pop();
+        } else {
+          object[string] = {};          
+        }
+        break;
+      case '}':
         idStack.pop();
         relationshipStack.pop();
-      }
-      if (char === ',') {
+        if (!string) {
+          return string;
+        }
         object[string] = this.props.items[id][string];
+        break;
+      case ',':
+        if (!string) {
+          return string;
+        }
+        object[string] = this.props.items[id][string];
+        break;
       }
       return '';
     },'');
--- test/dynamic/DynamicContainer.spec.js
+++ test/dynamic/DynamicContainer.spec.js
@@ -15,25 +15,34 @@ describe('DynamicContainer', () => {
         2: {
           type: 'user',
           name: 'Janice',
+          likes: [4,5],
           employer: 3
         },
         3: {
           type: 'employer',
           name: 'Coral'
+        },
+        4: {
+          type: 'like',
+          name: 'Regina'
+        },
+        5: {
+          type: 'like',
+          name: 'Fatima'
         }
       }
     };
   });
   it('should retrieve objects based on a simple graphQL query', () => {
     let query = '{content}';
-    let output = new DynamicContainer(props).getPropsFromItems(query);
+    let output = new DynamicContainer(props).getPropsFromItems(query, 1);
     expect(output).to.deep.equal({
       content: 'stuff'
     });
   });
   it('should traverse the graph and return an appropriately formatted set of properties', () => {
     let query = '{content,author{name}}';
-    let output = new DynamicContainer(props).getPropsFromItems(query);
+    let output = new DynamicContainer(props).getPropsFromItems(query, 1);
     expect(output).to.deep.equal({
       content: 'stuff',
       author: {
@@ -43,13 +52,30 @@ describe('DynamicContainer', () => {
   });
   it('should traverse a deeply nested query', () => {
     let query = '{content,author{employer{name}}}';
-    let output = new DynamicContainer(props).getPropsFromItems(query);
+    let output = new DynamicContainer(props).getPropsFromItems(query, 1);
     expect(output).to.deep.equal({
       content: 'stuff',
       author: {
         employer:{
           name: 'Coral'
         }
+      }
+    });
+  });
+  it('should traverse a one to may relationship', ()=> {
+    const query = '{author{likes{name}},content}';
+    const output = new DynamicContainer(props).getPropsFromItems(query, 1);
+    expect(output).to.deep.equal({
+      content: 'stuff',
+      author: {
+        likes: [
+          {
+            name: 'Regina'
+          },
+          {
+            name: 'Fatima'
+          }
+        ]
       }
     });
   });
