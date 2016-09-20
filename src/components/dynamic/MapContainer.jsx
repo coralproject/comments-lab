f3ee12c2fd78358b9f4eadc05c727606e9074b7a
David Jay <davidgljay@gmail.com>
2016-09-20 13:22:16 -0400

    Adding documentation

--- src/components/dynamic/MapContainer.jsx
+++ src/components/dynamic/MapContainer.jsx
@@ -1,12 +1,26 @@
 import {Children} from 'react';
 
+/*
+* Maps a set of children onto an array of item ids
+* e.g. Displaying a stream of comments
+*
+* @props
+*   id- The id of the item with the property to be mapped.
+*   array- The property to be mapped. Should be an array of ids.
+*   items- All items in the redux store.
+*   config- An array of config objects.
+*/
+
 const MapContainer = (props) => {
   const itemArray = props.items[props.id][props.array];
-  return <div className={'map' + this.props.array}>
+  return <div className={'map' + props.array}>
     {
       itemArray.map((item) => {
         Children.map(props.children, (ChildComponent) => {
-          return <ChildComponent id={item}/>;
+          return <ChildComponent
+            id={item}
+            config={props.config}
+            items={props.items}/>;
         });
       })
     }
--- src/components/dynamic/RootContainer.jsx
+++ src/components/dynamic/RootContainer.jsx
@@ -1,5 +1,15 @@
 import {Children} from 'react';
 
+/*
+* Renders a set of dynamic components bases on a root id
+*
+* @props
+*   id- The root id of the application (e.g. an asset with several authors, comments, etc).
+*   type- The object type that this id should return (used for confirmation and readability).
+*   items- All items in the redux store.
+*   config- An array of config objects.
+*/
+
 const RootContainer = (props) => {
   if (props.items[props.id].type !== props.type) {
     console.warn('Id passed to RootContainer gets an object of an unexpected type. Expected ' + props.type + ' but got ' + props.items[props.id].type);
