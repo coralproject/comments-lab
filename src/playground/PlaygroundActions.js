d409c9caf2a37a3eb58749e0425a3ede6e933c31
David Jay <davidgljay@gmail.com>
2016-09-21 14:06:27 -0400

    Adding actions to get and put items.
    Still need to write tests

--- src/playground/PlaygroundActions.js
+++ src/playground/PlaygroundActions.js
@@ -1,4 +1,5 @@
 import uuid from 'uuid';
+import {fetchURL} from 'fetch';
 
 export const ADD_COMPONENT = 'ADD_COMPONENT';
 export const REMOVE_COMPONENT = 'REMOVE_COMPONENT';
@@ -16,6 +17,109 @@ export const SET_TOGGLER_GROUP = 'SET_TOGGLER_GROUP';
 export const TOGGLE_WELCOME_HERO = 'TOGGLE_WELCOME_HERO';
 export const SET_NUM_CHARS = 'SET_NUM_CHARS';
 export const SET_SNACKBAR = 'SET_SNACKBAR';
+export const ADD_ITEM = 'ADD_ITEM';
+
+export function addItem(item) {
+  return {
+    type: ADD_ITEM,
+    item
+  };
+}
+
+/*
+* Get Items from Query
+* Gets a set of items from a predefined query
+*
+* @params
+*   Query - a predefiend query for retreiving items
+*
+* @returns
+*   A promise resolving to a set of items
+*
+* @dispatches
+*   A set of items to the item store  
+*/
+export function getItemsFromQuery(query) {
+  return (dispatch) => {
+    return new Promise((resolve, reject) => {
+      fetchURL(query, (result) => {
+        if (result.statusCode != 200) {
+          reject(result);
+        } else {
+          for (var i = 0; i < result.items.length; i++) {
+            dispatch(addItem(result.items[i]));
+          }
+          resolve(result.items);
+        }
+      });
+    });
+  };
+}
+
+/*
+* Get Items Array
+* Gets a set of items from an array of item ids
+*
+* @params
+*   Query - a predefiend query for retreiving items
+*
+* @returns
+*   A promise resolving to a set of items
+*
+* @dispatches
+*   A set of items to the item store  
+*/
+
+export function getItemArray(ids) {
+  return (dispatch) => {
+    return new Promise((resolve, reject) => {
+      fetchURL('http://item.ids.query/' + ids, (result) => {
+        if (result.statusCode != 200) {
+          reject(result);
+        } else {
+          for (var i = 0; i < result.items.length; i++) {
+            dispatch(addItem(result.items[i]));
+          }
+          resolve(result.items);
+        }
+      });
+    });
+  };
+}
+
+/*
+* PutItem
+* Puts an item
+*
+* @params
+*   Item - the item to be put
+*
+* @returns
+*   A promise resolving to an item is
+*
+* @dispatches
+*   The newly put item to the item store
+*/
+
+export function putItem(item) {
+  return (dispatch) => {
+    return new Promise((resolve, reject) => {
+      let options = {
+        method: 'POST',
+        payload: JSON.stringify(item) 
+      };
+      fetchURL('http://item.post/', options, (result) => {
+        if (result.statusCode != 200) {
+          reject(result);
+        } else {
+          dispatch(addItem(Object.assign({}, item, {id: result.id})));
+          resolve(result.id);
+        }
+      });
+    });
+  };
+}
+
 
 export function addComponent(itemType, component, propTypes, order, configProps) {
   return {
